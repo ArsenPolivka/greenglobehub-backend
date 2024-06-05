fc160c62221b210e6fd8968b4d7c555c845aee6b
PolivkaArsen <arsen.polivka@tetalab.com>
2024-06-05 23:12:21 +0300

    ap/refactor-auth: user services refactored, articles error method created

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,61 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { createClient } from '@supabase/supabase-js';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
   private supabase;
-  private supabaseAdmin;
 
   constructor() {
     this.supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
-    this.supabaseAdmin = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE);
   }
 
-  async signUp(name: string, email: string, password: string, confirmPassword: string): Promise<any> {
+  async signUp(createAuthDto: CreateAuthDto): Promise<any> {
+    const { name, email, password, confirmPassword } = createAuthDto;
+
     if (password !== confirmPassword) {
-      throw new Error('Passwords do not match');
+      throw new BadRequestException('Passwords do not match');
     }
 
-    try {
-      const { data, error } = await this.supabase.auth.signUp({
-        email,
-        password,
-      });
+    const { data, error } = await this.supabase.auth.signUp({ email, password });
 
-      if (error) {
-        throw new Error(error.message);
-      }
-
-      const { data: userData, error: userError } = await this.supabase
-        .from('users')
-        .insert([
-          {
-            uniqueId: data.user.id,
-            name,
-            email: data.user.email,
-            registeredAt: new Date(),
-          }
-        ])
-        .select()
-
-      if (userError) {
-        throw new Error(userError.message);
-      }
-
-      return { userData, session: data.session };
-    } catch (error) {
-      throw new Error(error.message);
+    if (error) {
+      throw new BadRequestException(error.message);
     }
+
+    const { data: userData, error: userError } = await this.supabase
+      .from('users')
+      .insert([
+        {
+          uniqueId: data.user.id,
+          name,
+          email: data.user.email,
+          registeredAt: new Date(),
+        }
+      ])
+      .select()
+
+    if (userError) {
+      throw new BadRequestException(error.message);
+    }
+
+    return {
+      ...userData[0],
+      session: data.session.access_token
+    };
   }
 
-  async signIn(email: string, password: string): Promise<any> {
+  async signIn(signInDto: SignInDto): Promise<any> {
+    const { email, password } = signInDto;
+
     const { data, error } = await this.supabase.auth.signInWithPassword({
       email,
       password,
     });
 
     if (error) {
-      throw new Error(error.message);
+      throw new UnauthorizedException(error.message);
     }
 
     const { data: userData, error: userError } = await this.supabase
@@ -64,49 +65,54 @@ export class AuthService {
       .eq('email', email);
 
     if (userError) {
-      throw new Error(userError.message);
+      throw new BadRequestException(error.message);
     }
 
-    return { userData, session: data.session };
+    return {
+      ...userData[0],
+      session: data.session.access_token
+    };
   }
 
-  async getUser(sessionToken: string): Promise<any> {
+  async getUser(sessionToken: string): Promise<User> {
     const { data, error } = await this.supabase.auth.getUser(sessionToken);
 
     if (error) {
-      throw new Error(error.message);
+      throw new BadRequestException(error.message);
     }
 
     const { data: userData, error: userError } = await this.supabase
       .from('users')
       .select()
-      .eq('uniqueId', data.user.id);
+      .eq('uniqueId', data.user.id)
+      .single();
 
     if (userError) {
-      throw new Error(userError.message);
+      throw new BadRequestException(userError.message);
     }
 
-    return userData[0] || userData;
+    return userData;
   }
 
-  async getUserById(uniqueId: string): Promise<any> {
+  async getUserById(uniqueId: string): Promise<User> {
     const { data, error } = await this.supabase
       .from('users')
       .select()
-      .eq('uniqueId', uniqueId);
+      .eq('uniqueId', uniqueId)
+      .single();
 
     if (error) {
-      throw new Error(error.message);
+      throw new BadRequestException(error.message);
     }
 
-    return data[0] || data;
+    return data;
   }
 
   async logout(sessionToken: string): Promise<any> {
     const { error } = await this.supabase.auth.signOut(sessionToken);
 
     if (error) {
-      throw new Error(error.message);
+      throw new BadRequestException(error.message);
     }
 
     return { message: 'Logged out successfully' };
